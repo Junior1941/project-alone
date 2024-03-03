18d90a03c4cca48a5964dea5cecee83bd13c914c
Junior1941 <153503141+Junior1941@users.noreply.github.com>
2024-03-04 01:02:49 +0700

    Add files via upload

--- back-end/app.js
+++ back-end/app.js
@@ -1,25 +1,33 @@
-require('dotenv').config()
-const express = require('express')
-const cors = require('cors')
-const notFound = require('./middlewares/notFound')
-const errorMiddleware = require('./middlewares/error')
-const authRoute = require('./routes/auth-route')
-const todoRoute = require('./routes/todo-route')
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
+// const notFound = require("./middlewares/notFound");
+// const errorMiddleware = require("./middlewares/error");
+const userRoute = require("./src/routes/user-route");
+const authRoute = require("./src/routes/auth-route");
+const productRoute = require("./src/routes/product-route");
+const addressRoute = require("./src/routes/address-route");
+const cartRoute = require("./src/routes/cart-route");
+const orderRoute = require("./src/routes/order-route");
+const paymentRoute = require("./src/routes/payment-route");
 
-const app = express()
+const app = express();
 
-app.use(cors())
-app.use(express.json())
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 
-// service
-app.use('/auth', authRoute)
-app.use('/todos', todoRoute)
 
-// notFound
-app.use( notFound )
+// Use routers
+app.use("/users", userRoute);
+app.use("/orders", orderRoute);
+app.use("/carts", cartRoute);
+app.use("/addresses", addressRoute);
+app.use("/products", productRoute);
+app.use("/payments", paymentRoute);
+app.use("/auth", authRoute);
 
-// error
-app.use(errorMiddleware)
+app.use(express.static("public"));
 
-let port = process.env.PORT || 8000
-app.listen(port, ()=> console.log('Server on Port :', port))
+let port = process.env.PORT || 8000;
+app.listen(port, () => console.log("Server on Port :", port));
